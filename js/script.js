1a4ff3eea83014fe201ebabc0df580b97a56851e
bittoMazza <robertomazza99@gmail.com>
2022-06-29 13:51:28 +0200

    completed milestone 0

--- js/script.js
+++ js/script.js
@@ -1,22 +1,44 @@
 const images = [
-    'https://cdn.photographycourse.net/wp-content/uploads/2022/04/Portrait-vs-Landscape-Featured-Image-3.jpg',
-    'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6.jpg',
-    'https://cdn.photographycourse.net/wp-content/uploads/2014/11/Landscape-Photography-steps.jpg',
-    'https://www.adorama.com/alc/wp-content/uploads/2018/11/landscape-photography-tips-yosemite-valley-feature.jpg',
-    'https://iso.500px.com/wp-content/uploads/2021/02/Torres-Del-Paine-Sunset-By-Paul-Reiffer-2-1500x1000.jpg',
-    'https://mymodernmet.com/wp/wp-content/uploads/2020/02/Landscape-Photographer-of-the-Year-Sander-Grefte.jpg'
-    ];
+    {
+        url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
+        title: 'Svezia',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+
+    {
+        url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
+        title: 'Perù',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+
+    {
+        url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
+        title: 'Chile',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+    {
+        url: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
+        title: 'Argentina',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+    {
+        url: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop',
+        title: 'Colombia',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+];
 
 let activeIndex=0;
 let newImages;
 let newThumbImg;
 let thumbnailContainer = document.getElementById('thumbnail-container');
 let containerImages = document.getElementById('carousel-image-container');
+console.log(images.length)
 for(let i = 0;i < images.length; i++){
-    newThumbImg = document.createElement("img")
+    newThumbImg = document.createElement("img");
     newImages = document.createElement("img");
-    newThumbImg.setAttribute('src',images[i])
-    newImages.setAttribute('src',images[i]);
+    newThumbImg.setAttribute('src',images[i].url)
+    newImages.setAttribute('src',images[i].url);
     // Al primo elemento diamo la classe active
     if(i == 0){
         newThumbImg.classList.add('active');
@@ -28,8 +50,8 @@ for(let i = 0;i < images.length; i++){
 
 console.log(thumbnailContainer)
 // Con children creiamo una lista con tutti i figli del container 
-const listImage = containerImages.children;
-const listThumbnail = thumbnailContainer.children;
+const listImage = containerImages.querySelectorAll('img');
+const listThumbnail = thumbnailContainer.querySelectorAll('img');
 const btnNext = document.getElementById('next-btn');
 const btnPrev = document.getElementById('prev-btn');
 
